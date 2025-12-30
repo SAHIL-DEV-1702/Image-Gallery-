2b64746e30d655ab1a106f38308a073930146b0a
Sahil Patil <patilsahil@172002@gmail.com>
2025-12-30 13:38:47 +0530

    create ui from image gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([])
+
+
+  const getData = async () => {
+
+    const response = await axios.get('https://picsum.photos/v2/list?page=8&limit=100')
+    setData(response.data)
+    console.log('data fetch successfully');
+  }
+
+  let printData = <h3 className='text-gray-500'>USER DATA NOT FOUND</h3>
+
+  if (printData.length === 0) {
+    printData = data.map((e, idx) => <div key={idx}>{e.author}</div>)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-green-200 h-screen w-screen flex flex-col items-center">
+
+        <div className='border-2 border-red-100 h-200 w-350 m-5 flex flex-wrap overflow-y-scroll bg-blue-200'>
+
+          <div className='flex flex-wrap justify-center'>
+
+            {data.map((e, idx) =>
+              <div key={idx}>
+                <a href={e.url} target="_blank" rel="noreferrer">
+                  <div className="card h-48 w-48 m-2 border border-gray-300 rounded-md overflow-hidden flex ml-10 justify-center items-center" ><img src={e.download_url} alt="not found" className='h-48 w-48 overflow-hidden' /></div>
+
+                  <div className="text-center mt-2" >{e.author}</div>
+                </a>
+              </div>
+            )}
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
